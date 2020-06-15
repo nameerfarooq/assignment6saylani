e330e1fc09b765a9e87a48fb824bf57b06dcd060
nameerfarooq <nameerfarooquit@gmail.com>
2020-06-16 00:47:36 +0500

    ready

---
+++ app.js
@@ -0,0 +1,60 @@
+// ........................ch 21-25.....................................................................
+// .....................................................................................................
+
+// q1
+
+// var f_name = String(prompt("enter your first name : "));
+// var l_name = String(prompt("enter your last name : "));
+
+// var FullName = f_name +" "+ l_name;
+// alert("welcome "+FullName);
+
+
+// q2
+// var mbl = String(prompt("enter your favourite mobile phone details : "));
+// a = mbl.length;
+// alert("my favourite mobile phone is : " + mbl + "\nlenght of string is : " + a );
+
+
+
+// q3
+// var country = "pakistan";
+// var a = country.indexOf('n');
+// alert("string : "+country+"\nindex on 'n' : "+a);
+
+
+// q4
+// var st = "Hello World";
+// var a = st.lastIndexOf('l');
+// alert("string : "+st+"\nindex on 'l' : "+a);
+
+
+// q5
+
+// var country = "pakistan";
+// var a = country.charAt(3)
+// alert("string : "+country+"\nCharacter at index 3  : "+a);
+
+
+// q6
+
+// var f_name = String(prompt("enter your first name : "));
+// var l_name = String(prompt("enter your last name : "));
+
+// var FullName = f_name.concat(l_name)
+// alert("welcome "+FullName);
+
+
+// q7
+//  var city = "hyderabad";
+//  var c =  city.replace('hyder','islam')
+//  alert(c)
+
+
+// q8
+// var message = 'Ali and Sami are best friends They play cricket and football together';
+// var a = message.replace(/and/g,'&');
+// alert(a)
+
+
+// 
